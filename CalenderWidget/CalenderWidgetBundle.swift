9a38dcfbda8d9c14b389fec6afc0dbb164db3ab4
hamza5645 <hamzaosama5645@outlook.com>
2025-01-13 23:45:39 +0200

    Created a Widget

---
+++ CalenderWidget/CalenderWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CalenderWidgetBundle.swift
+//  CalenderWidget
+//
+//  Created by Hamza Osama on 1/13/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CalenderWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CalenderWidget()
+    }
+}
