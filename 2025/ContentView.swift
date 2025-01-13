65edd550a83cf6c74e225fcac3256b52cfdf48f4
hamza5645 <hamzaosama5645@outlook.com>
2025-01-13 12:54:30 +0200

    Initial Commit

---
+++ 2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025
+//
+//  Created by Hamza Osama on 1/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025/_025App.swift
@@ -0,0 +1,17 @@
+//
+//  _025App.swift
+//  2025
+//
+//  Created by Hamza Osama on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct _025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
